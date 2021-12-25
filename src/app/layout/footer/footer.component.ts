64ac58d0fb290ff6d15d2a8a43b572cb3805f53b
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-25 15:16:12 +0100

    first commit

---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html'
+})
+export class FooterComponent {
+
+  festivalName = environment.festivalName;
+  year: number = new Date().getFullYear();
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  festivalName: "RMotownFestival",
+  apiBaseUrl: "https://nmotownfestivalapisg.azurewebsites.net/api/"
+};
